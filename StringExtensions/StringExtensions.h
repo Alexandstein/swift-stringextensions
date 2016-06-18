4d7bea248b471d6e8038afb5f542a3df64aff847
Alexandstein <alexand.stein@gmail.com>
2016-06-18 13:19:37 -0700

    Initial Commit

---
+++ StringExtensions/StringExtensions.h
@@ -0,0 +1,19 @@
+//
+//  StringExtensions.h
+//  StringExtensions
+//
+//  Created by Alex Stein on 6/18/16.
+//  Copyright © 2016 Alexander Stein. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for StringExtensions.
+FOUNDATION_EXPORT double StringExtensionsVersionNumber;
+
+//! Project version string for StringExtensions.
+FOUNDATION_EXPORT const unsigned char StringExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringExtensions/PublicHeader.h>
+
+
